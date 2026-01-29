c0c95d2b5a9741d3a6da54767845c6ec99b59b59
Valentin Dauvier <valentindauvier@hotmail.fr>
2026-01-29 16:34:48 +0100

    creation model Pokemon

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PokemonController;
 
 Route::get('/', function () {
     return view('home');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('pokemon', PokemonController::class);
